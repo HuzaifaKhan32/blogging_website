3c5c929bee209b212e703881f95d2c43eabc7a41
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-10-13 17:06:26 +0500

    Entering Dynamic Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import AI from "@/../public/AI.jpeg"
 import Tailwind from "@/../public/tailwind.png"
 import Programming from "@/../public/programming.png"
@@ -8,6 +8,38 @@ import Nextjs from "@/../public/nextjs.png"
 import Typescript from "@/../public/typescript.png"
 import GSAP from "@/../public/GSAP.jpg"
 
+
+type Idata = {
+  title: string,
+  description: string,
+  image: StaticImageData
+}
+
+const data: Idata[] = [{
+  title: "Artificial Intelligence",
+  description: "Artificial Intelligence (AI) enables machines to mimic human intelligence for tasks like decision-making and learning. It's transforming industries by processing vast data and providing insights, promising smarter and more efficient systems for the future.",
+  image: AI
+}, {
+  title: "Tailwind CSS",
+  description: "Tailwind CSS is a utility-first framework that simplifies web design with predefined classes. It helps developers create responsive, customizable layouts quickly, focusing on flexibility and mobile-first design. Tailwind CSS is very useful in creating designs.",
+  image: Tailwind
+}, {
+  title: "What is Programming?",
+  description: "Programming is the process of writing code to instruct computers to perform tasks or solve problems. It uses languages like JavaScript or Python to create software and applications. Programmers write algorithms to automate processes and build digital solutions.",
+  image: Programming
+}, {
+  title: "Benefits Of Next.js",
+  description: "Next.js is a powerful React framework that enables server-side rendering and static site generation, improving performance and SEO. It simplifies routing and API integration, making it easy to build fast, scalable web applications.",
+  image: Nextjs
+}, {
+  title: "Typescript",
+  description: "TypeScript is a superset of JavaScript that adds static typing, helping developers catch errors during development. It improves code quality and makes large projects easier to manage. TypeScript is widely used for building scalable and reliable applications.",
+  image: Typescript
+}, {
+  title: "What is GSAP?",
+  description: "GSAP (GreenSock Animation Platform) is a JavaScript library for creating fast, high-performance animations on websites. It allows developers to animate elements like images and text with smooth transitions. GSAP is widely used for enhancing user interfaces with dynamic effects.",
+  image: GSAP
+},]
 const App = () => {
   return (
     <>
@@ -21,7 +53,25 @@ const App = () => {
       <div className="page2">
         <h1>Blogs</h1>
         <div className="parentContainer">
-          <div className="childContainer">
+          {data.map((elem) => (
+            <div className="childContainer">
+              <div className="imageContainer">
+                <Image
+                  className="image-AI"
+                  src={elem.image}
+                  width={200}
+                  height={200}
+                  alt="This picture is about AI"
+                />
+              </div>
+              <div className="title">{elem.title}</div>
+              <p>{elem.description}</p>
+              <div className="readmore-parent">
+                <Link href={"/blog1"} className="readmore">Read more</Link>
+              </div>
+            </div>
+          ))}
+          {/* <div className="childContainer">
             <div className="imageContainer">
               <Image
                 className="image-AI"
@@ -68,12 +118,12 @@ const App = () => {
             <div className="readmore-parent">
               <Link href={"/blog3"} className="readmore">Read more</Link>
             </div>
-          </div>
+          </div> */}
         </div>
-        <div className="parentContainer2">
+        {/* <div className="parentContainer2">
           <div className="childContainer">
             <div className="imageContainer">
-            <Image
+              <Image
                 className="image-AI"
                 src={Nextjs}
                 width={200}
@@ -89,7 +139,7 @@ const App = () => {
           </div>
           <div className="childContainer">
             <div className="imageContainer">
-            <Image
+              <Image
                 className="image-AI"
                 src={Typescript}
                 width={200}
@@ -105,7 +155,7 @@ const App = () => {
           </div>
           <div className="childContainer">
             <div className="imageContainer">
-            <Image
+              <Image
                 className="image-AI"
                 src={GSAP}
                 width={200}
@@ -119,7 +169,7 @@ const App = () => {
               <Link href={"/blog6"} className="readmore">Read more</Link>
             </div>
           </div>
-        </div>
+        </div> */}
       </div >
     </>
   )
